fbd0a138f7a5e4e4d54d9fc8e79ca375b0556ec5
Ramil Zaripov <zarram89@gmail.com>
2024-09-09 18:04:59 +0500

    Исправляет кавычки в моковых данных

--- src/mock/offer.js
+++ src/mock/offer.js
@@ -1,205 +1,205 @@
 const mockOffers = [
   {
-    "type": "taxi",
-    "offers": [
+    'type': 'taxi',
+    'offers': [
       {
-        "id": "605b2d65-a231-4ed8-8823-77adc29fd26a",
-        "title": "Upgrade to a business class",
-        "price": 56
+        'id': '605b2d65-a231-4ed8-8823-77adc29fd26a',
+        'title': 'Upgrade to a business class',
+        'price': 56
       },
       {
-        "id": "09fa24e8-b682-4d11-a1a4-4b291278425d",
-        "title": "Choose the radio station",
-        "price": 128
+        'id': '09fa24e8-b682-4d11-a1a4-4b291278425d',
+        'title': 'Choose the radio station',
+        'price': 128
       },
       {
-        "id": "b89150ae-06b9-43c7-a32f-a2f209b63723",
-        "title": "Choose temperature",
-        "price": 87
+        'id': 'b89150ae-06b9-43c7-a32f-a2f209b63723',
+        'title': 'Choose temperature',
+        'price': 87
       },
       {
-        "id": "6a6b9c37-813e-4dd2-8c1e-f166cea2aa24",
-        "title": "Drive quickly, I'm in a hurry",
-        "price": 155
+        'id': '6a6b9c37-813e-4dd2-8c1e-f166cea2aa24',
+        'title': 'Drive quickly, I\'m in a hurry',
+        'price': 155
       },
       {
-        "id": "c59b78ac-d5fb-4f88-8034-ceaae508d2e4",
-        "title": "Drive slowly",
-        "price": 66
+        'id': 'c59b78ac-d5fb-4f88-8034-ceaae508d2e4',
+        'title': 'Drive slowly',
+        'price': 66
       }
     ]
   },
   {
-    "type": "bus",
-    "offers": [
+    'type': 'bus',
+    'offers': [
       {
-        "id": "095fb1bc-707a-436e-beae-04449d3acb07",
-        "title": "Infotainment system",
-        "price": 71
+        'id': '095fb1bc-707a-436e-beae-04449d3acb07',
+        'title': 'Infotainment system',
+        'price': 71
       },
       {
-        "id": "6147bc88-8de5-48af-a832-bfe828f81dd0",
-        "title": "Order meal",
-        "price": 195
+        'id': '6147bc88-8de5-48af-a832-bfe828f81dd0',
+        'title': 'Order meal',
+        'price': 195
       },
       {
-        "id": "575ef478-35fb-404b-bc85-ef9bfd87ff2a",
-        "title": "Choose seats",
-        "price": 45
+        'id': '575ef478-35fb-404b-bc85-ef9bfd87ff2a',
+        'title': 'Choose seats',
+        'price': 45
       }
     ]
   },
   {
-    "type": "train",
-    "offers": [
+    'type': 'train',
+    'offers': [
       {
-        "id": "1b737f5d-522d-460c-9e94-11398dfc9e3d",
-        "title": "Book a taxi at the arrival point",
-        "price": 93
+        'id': '1b737f5d-522d-460c-9e94-11398dfc9e3d',
+        'title': 'Book a taxi at the arrival point',
+        'price': 93
       },
       {
-        "id": "f1b216c3-ba92-4856-b31a-d960607de977",
-        "title": "Order a breakfast",
-        "price": 34
+        'id': 'f1b216c3-ba92-4856-b31a-d960607de977',
+        'title': 'Order a breakfast',
+        'price': 34
       },
       {
-        "id": "1714f4b4-1bca-4cb0-9302-f5855980eefe",
-        "title": "Wake up at a certain time",
-        "price": 117
+        'id': '1714f4b4-1bca-4cb0-9302-f5855980eefe',
+        'title': 'Wake up at a certain time',
+        'price': 117
       }
     ]
   },
   {
-    "type": "flight",
-    "offers": [
+    'type': 'flight',
+    'offers': [
       {
-        "id": "ee39cecf-bd06-40c3-8daa-bef799ca4f59",
-        "title": "Choose meal",
-        "price": 154
+        'id': 'ee39cecf-bd06-40c3-8daa-bef799ca4f59',
+        'title': 'Choose meal',
+        'price': 154
       },
       {
-        "id": "2c9898c7-b1e9-44ad-be51-e9ce28d01120",
-        "title": "Choose seats",
-        "price": 120
+        'id': '2c9898c7-b1e9-44ad-be51-e9ce28d01120',
+        'title': 'Choose seats',
+        'price': 120
       },
       {
-        "id": "7bef7ef8-223c-4945-9f9d-daac85eabfd9",
-        "title": "Upgrade to comfort class",
-        "price": 94
+        'id': '7bef7ef8-223c-4945-9f9d-daac85eabfd9',
+        'title': 'Upgrade to comfort class',
+        'price': 94
       },
       {
-        "id": "9b2abd8a-cded-4e53-b13f-8a7c24a61d93",
-        "title": "Upgrade to business class",
-        "price": 157
+        'id': '9b2abd8a-cded-4e53-b13f-8a7c24a61d93',
+        'title': 'Upgrade to business class',
+        'price': 157
       },
       {
-        "id": "cbab2ac1-f371-40c5-b00e-cd6afb26a547",
-        "title": "Add luggage",
-        "price": 168
+        'id': 'cbab2ac1-f371-40c5-b00e-cd6afb26a547',
+        'title': 'Add luggage',
+        'price': 168
       },
       {
-        "id": "1705434e-2ad5-4276-8048-25441ece631f",
-        "title": "Business lounge",
-        "price": 37
+        'id': '1705434e-2ad5-4276-8048-25441ece631f',
+        'title': 'Business lounge',
+        'price': 37
       }
     ]
   },
   {
-    "type": "check-in",
-    "offers": [
+    'type': 'check-in',
+    'offers': [
       {
-        "id": "77a26089-c3ce-4f8d-9b49-70c5197ea02e",
-        "title": "Choose the time of check-in",
-        "price": 116
+        'id': '77a26089-c3ce-4f8d-9b49-70c5197ea02e',
+        'title': 'Choose the time of check-in',
+        'price': 116
       },
       {
-        "id": "7275cbad-122b-4b17-a0ea-6d960eef2b97",
-        "title": "Choose the time of check-out",
-        "price": 92
+        'id': '7275cbad-122b-4b17-a0ea-6d960eef2b97',
+        'title': 'Choose the time of check-out',
+        'price': 92
       },
       {
-        "id": "b62080b2-42f6-43a7-9dfd-f749ce32a40e",
-        "title": "Add breakfast",
-        "price": 169
+        'id': 'b62080b2-42f6-43a7-9dfd-f749ce32a40e',
+        'title': 'Add breakfast',
+        'price': 169
       },
       {
-        "id": "55eaf254-ea26-49f5-b708-02d562f2ff50",
-        "title": "Laundry",
-        "price": 105
+        'id': '55eaf254-ea26-49f5-b708-02d562f2ff50',
+        'title': 'Laundry',
+        'price': 105
       },
       {
-        "id": "dfb2196a-3d0f-4cf3-8e28-2e9372d12e7b",
-        "title": "Order a meal from the restaurant",
-        "price": 124
+        'id': 'dfb2196a-3d0f-4cf3-8e28-2e9372d12e7b',
+        'title': 'Order a meal from the restaurant',
+        'price': 124
       }
     ]
   },
   {
-    "type": "sightseeing",
-    "offers": []
+    'type': 'sightseeing',
+    'offers': []
   },
   {
-    "type": "ship",
-    "offers": [
+    'type': 'ship',
+    'offers': [
       {
-        "id": "83dcb532-a0f9-49ea-bbc7-55b48da83f4d",
-        "title": "Choose meal",
-        "price": 160
+        'id': '83dcb532-a0f9-49ea-bbc7-55b48da83f4d',
+        'title': 'Choose meal',
+        'price': 160
       },
       {
-        "id": "9a040cfd-14fe-4967-9a12-389ff2a98683",
-        "title": "Choose seats",
-        "price": 172
+        'id': '9a040cfd-14fe-4967-9a12-389ff2a98683',
+        'title': 'Choose seats',
+        'price': 172
       },
       {
-        "id": "9043c8fd-9f4a-4f9e-b97c-e854e8e7daef",
-        "title": "Upgrade to comfort class",
-        "price": 123
+        'id': '9043c8fd-9f4a-4f9e-b97c-e854e8e7daef',
+        'title': 'Upgrade to comfort class',
+        'price': 123
       },
       {
-        "id": "19ef9851-caa9-4530-a8fe-dc03e7b0485a",
-        "title": "Upgrade to business class",
-        "price": 143
+        'id': '19ef9851-caa9-4530-a8fe-dc03e7b0485a',
+        'title': 'Upgrade to business class',
+        'price': 143
       },
       {
-        "id": "87b6c88d-99d6-46c7-aeaa-348607d5f870",
-        "title": "Add luggage",
-        "price": 88
+        'id': '87b6c88d-99d6-46c7-aeaa-348607d5f870',
+        'title': 'Add luggage',
+        'price': 88
       },
       {
-        "id": "3115fd86-e4ff-40a5-8ff8-0ba4bd397a1e",
-        "title": "Business lounge",
-        "price": 127
+        'id': '3115fd86-e4ff-40a5-8ff8-0ba4bd397a1e',
+        'title': 'Business lounge',
+        'price': 127
       }
     ]
   },
   {
-    "type": "drive",
-    "offers": [
+    'type': 'drive',
+    'offers': [
       {
-        "id": "5cceca48-e7cc-45f8-97f6-b552629f2d0a",
-        "title": "With automatic transmission",
-        "price": 109
+        'id': '5cceca48-e7cc-45f8-97f6-b552629f2d0a',
+        'title': 'With automatic transmission',
+        'price': 109
       },
       {
-        "id": "f44bd763-6d68-4aa0-a694-b5adebc08565",
-        "title": "With air conditioning",
-        "price": 193
+        'id': 'f44bd763-6d68-4aa0-a694-b5adebc08565',
+        'title': 'With air conditioning',
+        'price': 193
       }
     ]
   },
   {
-    "type": "restaurant",
-    "offers": [
+    'type': 'restaurant',
+    'offers': [
       {
-        "id": "943c13cf-4dcb-4f84-8394-8acd2b2e67ba",
-        "title": "Choose live music",
-        "price": 138
+        'id': '943c13cf-4dcb-4f84-8394-8acd2b2e67ba',
+        'title': 'Choose live music',
+        'price': 138
       },
       {
-        "id": "76e2af55-252a-4035-9732-0d2ff2b49db2",
-        "title": "Choose VIP area",
-        "price": 90
+        'id': '76e2af55-252a-4035-9732-0d2ff2b49db2',
+        'title': 'Choose VIP area',
+        'price': 90
       }
     ]
   }
