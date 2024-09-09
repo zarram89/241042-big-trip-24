f7b799254388681c1d8a19b7afc9394ac7f5f314
Ramil Zaripov <zarram89@gmail.com>
2024-09-09 17:52:17 +0500

    Добавит моковые данные

---
+++ src/mock/destination.js
@@ -0,0 +1,158 @@
+const mockDestinations = [
+  {
+    "id": "1cf86b06-e5d8-4fb0-9084-dff6faaf6d18",
+    "description": "Den Haag - for those who value comfort and coziness",
+    "name": "Den Haag",
+    "pictures": [
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/1.jpg",
+        "description": "Den Haag a perfect place to stay with a family"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/18.jpg",
+        "description": "Den Haag a perfect place to stay with a family"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/9.jpg",
+        "description": "Den Haag famous for its crowded street markets with the best street food in Asia"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/2.jpg",
+        "description": "Den Haag is a beautiful city"
+      }
+    ]
+  },
+  {
+    "id": "e385a3ce-2e95-4262-989f-51799f504408",
+    "description": "Madrid - with a beautiful old town",
+    "name": "Madrid",
+    "pictures": [
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/15.jpg",
+        "description": "Madrid is a beautiful city"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/15.jpg",
+        "description": "Madrid full of of cozy canteens where you can try the best coffee in the Middle East"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/5.jpg",
+        "description": "Madrid for those who value comfort and coziness"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/10.jpg",
+        "description": "Madrid with crowded streets"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/11.jpg",
+        "description": "Madrid famous for its crowded street markets with the best street food in Asia"
+      }
+    ]
+  },
+  {
+    "id": "8cbb6d07-3758-40a9-b48d-973d8f07e6cd",
+    "description": "Hiroshima - for those who value comfort and coziness",
+    "name": "Hiroshima",
+    "pictures": []
+  },
+  {
+    "id": "e6ca0dad-5322-439f-a793-4e56e4b8458a",
+    "description": "Rome - a true asian pearl",
+    "name": "Rome",
+    "pictures": []
+  },
+  {
+    "id": "733a7d11-615f-4421-b548-7ad7832b79c1",
+    "description": "Rotterdam - middle-eastern paradise",
+    "name": "Rotterdam",
+    "pictures": [
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/1.jpg",
+        "description": "Rotterdam with an embankment of a mighty river as a centre of attraction"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/20.jpg",
+        "description": "Rotterdam in a middle of Europe"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/13.jpg",
+        "description": "Rotterdam full of of cozy canteens where you can try the best coffee in the Middle East"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/2.jpg",
+        "description": "Rotterdam full of of cozy canteens where you can try the best coffee in the Middle East"
+      }
+    ]
+  },
+  {
+    "id": "2c5a64ff-3d19-4961-8126-85fad7240df8",
+    "description": "",
+    "name": "Barcelona",
+    "pictures": []
+  },
+  {
+    "id": "b4e50dbd-9525-48cd-a47f-67cc08025912",
+    "description": "Nagasaki - a true asian pearl",
+    "name": "Nagasaki",
+    "pictures": [
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/11.jpg",
+        "description": "Nagasaki a true asian pearl"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/2.jpg",
+        "description": "Nagasaki a true asian pearl"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/6.jpg",
+        "description": "Nagasaki for those who value comfort and coziness"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/9.jpg",
+        "description": "Nagasaki famous for its crowded street markets with the best street food in Asia"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/7.jpg",
+        "description": "Nagasaki full of of cozy canteens where you can try the best coffee in the Middle East"
+      }
+    ]
+  },
+  {
+    "id": "f3ce1e9f-d82c-4ebd-b93d-15f48e2baadb",
+    "description": "Amsterdam - famous for its crowded street markets with the best street food in Asia",
+    "name": "Amsterdam",
+    "pictures": []
+  },
+  {
+    "id": "e9d6f17b-c3f3-4445-b98e-74bdeaee2e9b",
+    "description": "Paris - a perfect place to stay with a family",
+    "name": "Paris",
+    "pictures": [
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/10.jpg",
+        "description": "Paris is a beautiful city"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/19.jpg",
+        "description": "Paris in a middle of Europe"
+      },
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/3.jpg",
+        "description": "Paris full of of cozy canteens where you can try the best coffee in the Middle East"
+      }
+    ]
+  },
+  {
+    "id": "e3e9c36e-3116-4046-bf6e-ba3d2a55b22d",
+    "description": "Valencia - for those who value comfort and coziness",
+    "name": "Valencia",
+    "pictures": [
+      {
+        "src": "https://24.objects.htmlacademy.pro/static/destinations/5.jpg",
+        "description": "Valencia full of of cozy canteens where you can try the best coffee in the Middle East"
+      }
+    ]
+  }
+];
+
+export {mockDestinations};
--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,8 @@ function capitalizeFirstLetter(value) {
   return value[0].toUpperCase() + value.slice(1);
 }
 
-export {capitalizeFirstLetter};
+function getRandomArrayElement(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+export {capitalizeFirstLetter, getRandomArrayElement};
