69124b0a856a3301843c520e3903f4480110f6ab
Ramil Zaripov <zarram89@gmail.com>
2024-09-09 18:48:02 +0500

    Подключит модель

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import TripInfo from './view/trip-info-view';
 import FilterView from './view/filter-view';
 import EventsListPresenter from './presenter/events-list-presenter';
 import { render, RenderPosition } from './render';
+import PointsModel from './model/points';
 
 const tripHeaderElement = document.querySelector('.page-header');
 const tripTripMainElement = tripHeaderElement.querySelector('.trip-main');
 const tripFiltersElement = tripHeaderElement.querySelector('.trip-controls__filters');
 const tripMainElement = document.querySelector('.page-main');
 const tripEventsElement = tripMainElement.querySelector('.trip-events');
-const eventsListPresenter = new EventsListPresenter({container: tripEventsElement});
+
+const pointsModel = new PointsModel();
+const eventsListPresenter = new EventsListPresenter({container: tripEventsElement, pointsModel});
 
 render(new TripInfo(), tripTripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), tripFiltersElement);
--- src/presenter/events-list-presenter.js
+++ src/presenter/events-list-presenter.js
@@ -1,25 +1,27 @@
 import EventsListView from '../view/events-list-view';
 import SortView from '../view/sort-view';
 import EditPointView from '../view/edit-point-view';
-import NewPointView from '../view/new-point-view';
+// import NewPointView from '../view/new-point-view';
 import PointView from '../view/point-view';
 import { render } from '../render';
 
 export default class EventsListPresenter {
   eventsListComponent = new EventsListView();
 
-  constructor({container}) {
+  constructor({container, pointsModel}) {
     this.eventsListContainer = container;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.eventsListPoints = [...this.pointsModel.getPoints()];
     render(this.eventsListComponent, this.eventsListContainer);
     render(new SortView(), this.eventsListComponent.getElement());
-    render(new EditPointView(), this.eventsListComponent.getElement());
-    render(new NewPointView(), this.eventsListComponent.getElement());
+    render(new EditPointView({point: this.eventsListPoints[0]}), this.eventsListComponent.getElement());
+    // render(new NewPointView(), this.eventsListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new PointView(), this.eventsListComponent.getElement());
+    for (let i = 1; i < this.eventsListPoints.length; i++) {
+      render(new PointView({point: this.eventsListPoints[i]}), this.eventsListComponent.getElement());
     }
   }
 }
